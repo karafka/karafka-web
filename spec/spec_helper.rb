705bb3d964b506f2777a50bd88d3710dafd79306
Maciej Mensfeld <maciej@mensfeld.pl>
2023-02-23 14:33:39 +0100

    spec and factory bot setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.order = :random
+  config.include FactoryBot::Syntax::Methods
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
