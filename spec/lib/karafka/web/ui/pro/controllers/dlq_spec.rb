052181f9377a46f35ece3663becdb98b02f8635f
Maciej Mensfeld <maciej@mensfeld.pl>
2023-08-12 15:44:12 +0200

    dlq specs

--- spec/lib/karafka/web/ui/pro/controllers/dlq_spec.rb
+++ spec/lib/karafka/web/ui/pro/controllers/dlq_spec.rb
@@ -5,11 +5,35 @@ RSpec.describe_current do
 
   describe '#index' do
     context 'when there are no dlq topics' do
-      pending
+      before { get 'dlq' }
+
+      it do
+        expect(response).to be_ok
+        expect(body).to include('No Dead Letter Queue topics exist in Kafka')
+        expect(body).to include(breadcrumbs)
+        expect(body).not_to include(pagination)
+        expect(body).not_to include(support_message)
+      end
     end
 
     context 'when there are dlq topics' do
-      pending
+      let(:topic) { Karafka::App.consumer_groups.first.topics.first }
+      let(:dlq_topic) { Karafka::App.consumer_groups.last.topics.first.name }
+
+      before do
+        allow(topic.dead_letter_queue).to receive(:topic).and_return(dlq_topic)
+
+        get 'dlq'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).to include(breadcrumbs)
+        expect(body).to include(dlq_topic)
+        expect(body).not_to include('No Dead Letter Queue topics exist in Kafka')
+        expect(body).not_to include(pagination)
+        expect(body).not_to include(support_message)
+      end
     end
   end
 end
