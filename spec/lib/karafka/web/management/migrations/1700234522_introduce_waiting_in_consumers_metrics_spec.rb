f276ffa5dfc4b8c1aaaa30e57cdbf56081a22af4
Maciej Mensfeld <maciej@mensfeld.pl>
2023-11-19 16:51:23 +0100

    add missing migrations specs (#216)

--- spec/lib/karafka/web/management/migrations/1700234522_introduce_waiting_in_consumers_metrics_spec.rb
+++ spec/lib/karafka/web/management/migrations/1700234522_introduce_waiting_in_consumers_metrics_spec.rb
@@ -4,5 +4,18 @@ RSpec.describe Karafka::Web::Management::Migrations::IntroduceWaitingInConsumers
   it { expect(described_class.versions_until).to eq('1.1.1') }
   it { expect(described_class.type).to eq(:consumers_metrics) }
 
-  pending
+  context 'when migrating from 1.0.0' do
+    let(:state) { Fixtures.json('consumers_metrics_v1.0.0') }
+    let(:times) { %i[days hours minutes seconds] }
+
+    before { described_class.new.migrate(state) }
+
+    it 'expect to add waiting to all aggregated' do
+      times.each do |key_name|
+        state[:aggregated][key_name].each do |sample|
+          expect(sample.last[:waiting]).to eq(0)
+        end
+      end
+    end
+  end
 end
--- spec/lib/karafka/web/management/migrations/1700234522_remove_processing_from_consumers_state_spec.rb
+++ spec/lib/karafka/web/management/migrations/1700234522_remove_processing_from_consumers_state_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe Karafka::Web::Management::Migrations::RemoveProcessingFromConsume
   it { expect(described_class.versions_until).to eq('1.2.1') }
   it { expect(described_class.type).to eq(:consumers_state) }
 
-  pending
+  context 'when migrating from 1.1.0' do
+    let(:state) { Fixtures.json('consumers_state_v1.1.0') }
+
+    before { described_class.new.migrate(state) }
+
+    it { expect(state[:stats].key?(:processing)).to eq(false) }
+  end
 end
