ee5c870c857f3f0e602456201e9f8e9e0c34ca5d
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-19 13:30:32 +0100

    remarks

--- spec/lib/karafka/web/tracking/sampler_spec.rb
+++ spec/lib/karafka/web/tracking/sampler_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe_current do
   it { expect(sampler.ruby_version).to start_with('ruby ') }
   it { expect(sampler.karafka_version).to include('2.3.') }
   it { expect(sampler.karafka_web_version).to include('0.9.') }
-  it { expect(sampler.karafka_core_version).to include('2.3.') }
+  it { expect(sampler.karafka_core_version).to include('2.4.') }
   it { expect(sampler.rdkafka_version).to start_with('0.1') }
   it { expect(sampler.librdkafka_version).to start_with('2.3') }
   it { expect(sampler.waterdrop_version).to start_with('2.') }
