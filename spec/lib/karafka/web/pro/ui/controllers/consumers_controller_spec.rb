9bc0e0cb25d4f3dd431be5cc6e88b004420c319b
Maciej Mensfeld <maciej@mensfeld.pl>
2024-04-07 21:07:37 +0200

    more specs

--- spec/lib/karafka/web/pro/ui/controllers/consumers_controller_spec.rb
+++ spec/lib/karafka/web/pro/ui/controllers/consumers_controller_spec.rb
@@ -221,6 +221,340 @@ RSpec.describe_current do
     end
   end
 
+  describe '#performance' do
+    context 'when the state data is missing' do
+      before do
+        topics_config.consumers.states = states_topic
+
+        get 'consumers/performance'
+      end
+
+      it do
+        expect(response).not_to be_ok
+        expect(status).to eq(404)
+      end
+    end
+
+    context 'when there are no active consumers' do
+      before do
+        topics_config.consumers.reports = states_topic
+
+        get 'consumers/performance'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include(no_processes)
+      end
+    end
+
+    context 'when commanding is disabled' do
+      before do
+        Karafka::Web.config.commanding.active = false
+
+        get 'consumers/performance'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include('Controls')
+        expect(body).not_to include('Commands')
+      end
+    end
+
+    context 'when commanding is enabled' do
+      before do
+        Karafka::Web.config.commanding.active = true
+
+        get 'consumers/performance'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).to include('Controls')
+        expect(body).to include('Commands')
+      end
+    end
+
+    context 'when there are active consumers' do
+      before { get 'consumers/performance' }
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(no_processes)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include('shinra:1:1')
+        expect(body).to include('/consumers/shinra:1:1/subscriptions')
+        expect(body).to include('RSS')
+        expect(body).to include('ID')
+        expect(body).to include('Utilization')
+        expect(body).to include('Threads')
+        expect(body).to include('120 MB')
+        expect(body).to include('5.6%')
+      end
+
+      context 'when sorting' do
+        before { get 'consumers/performance?sort=id+desc' }
+
+        it { expect(response).to be_ok }
+      end
+    end
+
+    context 'when there are active consumers reported in a transactional fashion' do
+      before do
+        topics_config.consumers.states = states_topic
+        topics_config.consumers.reports = reports_topic
+
+        produce(states_topic, Fixtures.consumers_states_file, type: :transactional)
+        produce(reports_topic, Fixtures.consumers_reports_file, type: :transactional)
+
+        get 'consumers/performance'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(no_processes)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include('shinra:1:1')
+        expect(body).to include('/consumers/shinra:1:1/subscriptions')
+        expect(body).to include('RSS')
+        expect(body).to include('ID')
+        expect(body).to include('Utilization')
+        expect(body).to include('Threads')
+        expect(body).to include('120 MB')
+        expect(body).to include('5.6%')
+      end
+    end
+
+    context 'when there are more consumers that we fit in a single page' do
+      before do
+        topics_config.consumers.states = states_topic
+        topics_config.consumers.reports = reports_topic
+
+        data = Fixtures.consumers_states_json(symbolize_names: false)
+        base_report = Fixtures.consumers_reports_json(symbolize_names: false)
+
+        100.times do |i|
+          id = "shinra:#{i}:#{i}"
+
+          data['processes'][id] = {
+            dispatched_at: 2_690_818_669.526_218,
+            offset: i
+          }
+
+          report = base_report.dup
+          report['process']['id'] = id
+
+          produce(reports_topic, report.to_json, key: id)
+        end
+
+        produce(states_topic, data.to_json)
+      end
+
+      context 'when we visit first page' do
+        before { get 'consumers/performance' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include('shinra:0:0')
+          expect(body).to include('shinra:1:1')
+          expect(body).to include('shinra:11:11')
+          expect(body).to include('shinra:12:12')
+          expect(body.scan('shinra:').size).to eq(50)
+          expect(body).not_to include(support_message)
+        end
+      end
+
+      context 'when we visit second page' do
+        before { get 'consumers/performance?page=2' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include('shinra:32:32')
+          expect(body).to include('shinra:34:34')
+          expect(body).to include('shinra:35:35')
+          expect(body).to include('shinra:35:35')
+          expect(body.scan('shinra:').size).to eq(50)
+          expect(body).not_to include(support_message)
+        end
+      end
+
+      context 'when we go beyond available pages' do
+        before { get 'consumers/performance?page=100' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include(no_meaningful_results)
+          expect(body.scan('shinra:').size).to eq(0)
+          expect(body).not_to include(support_message)
+        end
+      end
+    end
+  end
+
+  describe '#controls' do
+    context 'when the state data is missing' do
+      before do
+        topics_config.consumers.states = states_topic
+
+        get 'consumers/controls'
+      end
+
+      it do
+        expect(response).not_to be_ok
+        expect(status).to eq(404)
+      end
+    end
+
+    context 'when there are no active consumers' do
+      before do
+        topics_config.consumers.reports = states_topic
+
+        get 'consumers/controls'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include(no_processes)
+      end
+    end
+
+    context 'when there are active consumers' do
+      before { get 'consumers/controls' }
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(no_processes)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include('shinra:1:1')
+        expect(body).to include('/consumers/shinra:1:1/subscriptions')
+        expect(body).to include('running')
+        expect(body).to include('ID')
+        expect(body).to include('Performance')
+        expect(body).to include('Quiet All')
+        expect(body).to include('Stop All')
+        expect(body).to include('Probe')
+      end
+
+      context 'when sorting' do
+        before { get 'consumers/controls?sort=id+desc' }
+
+        it { expect(response).to be_ok }
+      end
+    end
+
+    context 'when there are active consumers reported in a transactional fashion' do
+      before do
+        topics_config.consumers.states = states_topic
+        topics_config.consumers.reports = reports_topic
+
+        produce(states_topic, Fixtures.consumers_states_file, type: :transactional)
+        produce(reports_topic, Fixtures.consumers_reports_file, type: :transactional)
+
+        get 'consumers/controls'
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(body).not_to include(support_message)
+        expect(body).not_to include(no_processes)
+        expect(body).not_to include(pagination)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include('shinra:1:1')
+        expect(body).to include('/consumers/shinra:1:1/subscriptions')
+        expect(body).to include('running')
+        expect(body).to include('ID')
+        expect(body).to include('Performance')
+        expect(body).to include('Quiet All')
+        expect(body).to include('Stop All')
+        expect(body).to include('Probe')
+      end
+    end
+
+    context 'when there are more consumers that we fit in a single page' do
+      before do
+        topics_config.consumers.states = states_topic
+        topics_config.consumers.reports = reports_topic
+
+        data = Fixtures.consumers_states_json(symbolize_names: false)
+        base_report = Fixtures.consumers_reports_json(symbolize_names: false)
+
+        100.times do |i|
+          id = "shinra:#{i}:#{i}"
+
+          data['processes'][id] = {
+            dispatched_at: 2_690_818_669.526_218,
+            offset: i
+          }
+
+          report = base_report.dup
+          report['process']['id'] = id
+
+          produce(reports_topic, report.to_json, key: id)
+        end
+
+        produce(states_topic, data.to_json)
+      end
+
+      context 'when we visit first page' do
+        before { get 'consumers/controls' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include('shinra:0:0')
+          expect(body).to include('shinra:1:1')
+          expect(body).to include('shinra:11:11')
+          expect(body).to include('shinra:12:12')
+          expect(body.scan('shinra:').size).to eq(125)
+          expect(body).not_to include(support_message)
+        end
+      end
+
+      context 'when we visit second page' do
+        before { get 'consumers/controls?page=2' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include('shinra:32:32')
+          expect(body).to include('shinra:34:34')
+          expect(body).to include('shinra:35:35')
+          expect(body).to include('shinra:35:35')
+          expect(body.scan('shinra:').size).to eq(125)
+          expect(body).not_to include(support_message)
+        end
+      end
+
+      context 'when we go beyond available pages' do
+        before { get 'consumers/controls?page=100' }
+
+        it do
+          expect(response).to be_ok
+          expect(body).to include(pagination)
+          expect(body).to include(no_meaningful_results)
+          expect(body.scan('shinra:').size).to eq(0)
+          expect(body).not_to include(support_message)
+        end
+      end
+    end
+  end
+
   describe '#details' do
     context 'when details exist' do
       before { get 'consumers/shinra:1:1/details' }
