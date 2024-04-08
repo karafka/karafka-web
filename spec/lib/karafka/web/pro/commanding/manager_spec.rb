357cd1ef2ac20da39524a521d36d6d0618b9ce82
Maciej Mensfeld <maciej@mensfeld.pl>
2024-04-08 17:39:58 +0200

    more specs

--- spec/lib/karafka/web/pro/commanding/manager_spec.rb
+++ spec/lib/karafka/web/pro/commanding/manager_spec.rb
@@ -38,4 +38,81 @@ RSpec.describe_current do
       # Test passes if no deadlocks occur and method executes correctly
     end
   end
+
+  describe 'integration with commands' do
+    let(:listener) { Karafka::Web::Pro::Commanding::Listener.new }
+    let(:matcher) { Karafka::Web::Pro::Commanding::Matcher.new }
+    let(:unsupported_command_name) { 'unsupported_command' }
+
+    let(:message) do
+      instance_double(
+        'Karafka::Messages::Message',
+        payload: {
+          command: {
+            name: command_name
+          }
+        }
+      )
+    end
+
+    before do
+      allow(listener.class).to receive(:new).and_return(listener)
+      allow(matcher.class).to receive(:new).and_return(matcher)
+      allow(listener).to receive(:each).and_yield(message)
+      allow(matcher).to receive(:matches?).with(message).and_return(true)
+    end
+
+    context 'when command is probe' do
+      let(:probe_command) { Karafka::Web::Pro::Commanding::Commands::Probe.new }
+      let(:command_name) { 'probe' }
+
+      before do
+        allow(probe_command.class).to receive(:new).and_return(probe_command)
+        allow(probe_command).to receive(:call)
+      end
+
+      it 'executes probe command' do
+        manager.send(:call)
+        expect(probe_command).to have_received(:call)
+      end
+    end
+
+    context 'when command is quiet' do
+      let(:quiet_command) { Karafka::Web::Pro::Commanding::Commands::Quiet.new }
+      let(:command_name) { 'quiet' }
+
+      before do
+        allow(quiet_command.class).to receive(:new).and_return(quiet_command)
+        allow(quiet_command).to receive(:call)
+      end
+
+      it 'executes quiet command' do
+        manager.send(:call)
+        expect(quiet_command).to have_received(:call)
+      end
+    end
+
+    context 'when command is stop' do
+      let(:stop_command) { Karafka::Web::Pro::Commanding::Commands::Stop.new }
+      let(:command_name) { 'stop' }
+
+      before do
+        allow(stop_command.class).to receive(:new).and_return(stop_command)
+        allow(stop_command).to receive(:call)
+      end
+
+      it 'executes stop command' do
+        manager.send(:call)
+        expect(stop_command).to have_received(:call)
+      end
+    end
+
+    context 'when command is unsupported' do
+      let(:command_name) { unsupported_command_name }
+
+      it 'raises UnsupportedCaseError' do
+        expect { manager.send(:call) }.to raise_error(Karafka::Errors::UnsupportedCaseError)
+      end
+    end
+  end
 end
