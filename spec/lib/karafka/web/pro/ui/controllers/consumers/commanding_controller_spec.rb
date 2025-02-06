3f1a30f08f16116be65c7301f4a49a56edab9a25
Maciej Mensfeld <maciej@mensfeld.pl>
2025-02-06 20:41:35 +0100

    remarks

--- spec/lib/karafka/web/pro/ui/controllers/consumers/commanding_controller_spec.rb
+++ spec/lib/karafka/web/pro/ui/controllers/consumers/commanding_controller_spec.rb
@@ -26,9 +26,9 @@ RSpec.describe_current do
       message = Karafka::Admin.read_topic(commands_topic, 0, 1, -1).first
       expect(message.key).to eq(process_id)
       expect(message.payload[:schema_version]).to eq('1.1.0')
-      expect(message.payload[:type]).to eq('command')
+      expect(message.payload[:type]).to eq('request')
       expect(message.payload[:dispatched_at]).not_to be_nil
-      expect(message.payload[:command]).to eq(name: 'trace')
+      expect(message.payload[:command]).to eq(name: 'consumers.trace')
       expect(message.payload[:process]).to eq(id: process_id)
     end
   end
@@ -48,9 +48,9 @@ RSpec.describe_current do
       message = Karafka::Admin.read_topic(commands_topic, 0, 1, -1).first
       expect(message.key).to eq(process_id)
       expect(message.payload[:schema_version]).to eq('1.1.0')
-      expect(message.payload[:type]).to eq('command')
+      expect(message.payload[:type]).to eq('request')
       expect(message.payload[:dispatched_at]).not_to be_nil
-      expect(message.payload[:command]).to eq(name: 'quiet')
+      expect(message.payload[:command]).to eq(name: 'consumers.quiet')
       expect(message.payload[:process]).to eq(id: process_id)
     end
   end
@@ -70,9 +70,9 @@ RSpec.describe_current do
       message = Karafka::Admin.read_topic(commands_topic, 0, 1, -1).first
       expect(message.key).to eq(process_id)
       expect(message.payload[:schema_version]).to eq('1.1.0')
-      expect(message.payload[:type]).to eq('command')
+      expect(message.payload[:type]).to eq('request')
       expect(message.payload[:dispatched_at]).not_to be_nil
-      expect(message.payload[:command]).to eq(name: 'stop')
+      expect(message.payload[:command]).to eq(name: 'consumers.stop')
       expect(message.payload[:process]).to eq(id: process_id)
     end
   end
@@ -92,9 +92,9 @@ RSpec.describe_current do
       message = Karafka::Admin.read_topic(commands_topic, 0, 1, -1).first
       expect(message.key).to eq('*')
       expect(message.payload[:schema_version]).to eq('1.1.0')
-      expect(message.payload[:type]).to eq('command')
+      expect(message.payload[:type]).to eq('request')
       expect(message.payload[:dispatched_at]).not_to be_nil
-      expect(message.payload[:command]).to eq(name: 'quiet')
+      expect(message.payload[:command]).to eq(name: 'consumers.quiet')
       expect(message.payload[:process]).to eq(id: '*')
     end
   end
@@ -114,9 +114,9 @@ RSpec.describe_current do
       message = Karafka::Admin.read_topic(commands_topic, 0, 1, -1).first
       expect(message.key).to eq('*')
       expect(message.payload[:schema_version]).to eq('1.1.0')
-      expect(message.payload[:type]).to eq('command')
+      expect(message.payload[:type]).to eq('request')
       expect(message.payload[:dispatched_at]).not_to be_nil
-      expect(message.payload[:command]).to eq(name: 'stop')
+      expect(message.payload[:command]).to eq(name: 'consumers.stop')
       expect(message.payload[:process]).to eq(id: '*')
     end
   end
