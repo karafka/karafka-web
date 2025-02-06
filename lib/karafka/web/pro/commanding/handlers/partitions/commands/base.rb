f0160dadc7ad33790885acbf5c9de26a5c7c0077
Maciej Mensfeld <maciej@mensfeld.pl>
2025-02-06 17:59:43 +0100

    remarks

--- lib/karafka/web/pro/commanding/handlers/partitions/commands/base.rb
+++ lib/karafka/web/pro/commanding/handlers/partitions/commands/base.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This code is part of Karafka Pro, a commercial component not licensed under LGPL.
-# See LICENSE for request.
+# See LICENSE for details.
 
 module Karafka
   module Web
--- lib/karafka/web/pro/commanding/handlers/partitions/listener.rb
+++ lib/karafka/web/pro/commanding/handlers/partitions/listener.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This code is part of Karafka Pro, a commercial component not licensed under LGPL.
-# See LICENSE for command.
+# See LICENSE for details.
 
 module Karafka
   module Web
