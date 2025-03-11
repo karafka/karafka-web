32fbe4c33fa2546f2a571ee3f6168d389c24e78a
Maciej Mensfeld <maciej@mensfeld.pl>
2025-03-11 21:25:56 +0100

    reorg

--- spec/lib/karafka/web/ui/controllers/base_controller_spec.rb
+++ spec/lib/karafka/web/ui/controllers/base_controller_spec.rb
@@ -35,4 +35,146 @@ RSpec.describe_current do
       expect(status).to eq(402)
     end
   end
+
+  describe 'custom assets' do
+    let(:custom_css) { "assets/#{Karafka::Web::VERSION}/stylesheets/custom.css" }
+    let(:custom_js) { "assets/#{Karafka::Web::VERSION}/javascripts/custom.js" }
+
+    let(:ui_config) { Karafka::Web.config.ui }
+
+    after do
+      ui_config.custom_css = false
+      ui_config.custom_js = false
+    end
+
+    context 'when there is no custom css' do
+      before { get custom_css }
+
+      it do
+        expect(response).not_to be_ok
+        expect(status).to eq(404)
+      end
+    end
+
+    context 'when there is custom inline css' do
+      let(:css_content) { 'div { display: none }' }
+
+      before do
+        ui_config.custom_css = css_content
+
+        get custom_css
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(css_content)
+        expect(headers['content-type']).to eq('text/css')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+
+    context 'when there is custom css path that points to nothing' do
+      let(:css_content) { '/nothing/really' }
+
+      before do
+        ui_config.custom_css = css_content
+
+        get custom_css
+      end
+
+      it 'expect to treat is as a stringified content' do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(css_content)
+        expect(headers['content-type']).to eq('text/css')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+
+    context 'when there is custom css path that points to a file to show' do
+      let(:css_content) { File.join(Karafka::Web.gem_root, 'Gemfile') }
+
+      let(:fetched_content) { File.read(css_content) }
+
+      before do
+        ui_config.custom_css = css_content
+
+        get custom_css
+      end
+
+      it 'expect to treat is as a stringified content' do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(fetched_content)
+        expect(headers['content-type']).to eq('text/css')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+
+    context 'when there is no custom js' do
+      before { get custom_js }
+
+      it do
+        expect(response).not_to be_ok
+        expect(status).to eq(404)
+      end
+    end
+
+    context 'when there is custom inline js' do
+      let(:js_content) { 'div { display: none }' }
+
+      before do
+        ui_config.custom_js = js_content
+
+        get custom_js
+      end
+
+      it do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(js_content)
+        expect(headers['content-type']).to eq('application/javascript')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+
+    context 'when there is custom js path that points to nothing' do
+      let(:js_content) { '/nothing/really' }
+
+      before do
+        ui_config.custom_js = js_content
+
+        get custom_js
+      end
+
+      it 'expect to treat is as a stringified content' do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(js_content)
+        expect(headers['content-type']).to eq('application/javascript')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+
+    context 'when there is custom js path that points to a file to show' do
+      let(:js_content) { File.join(Karafka::Web.gem_root, 'Gemfile') }
+
+      let(:fetched_content) { File.read(js_content) }
+
+      before do
+        ui_config.custom_js = js_content
+
+        get custom_js
+      end
+
+      it 'expect to treat is as a stringified content' do
+        expect(response).to be_ok
+        expect(status).to eq(200)
+        expect(body).to eq(fetched_content)
+        expect(headers['content-type']).to eq('application/javascript')
+        expect(headers['cache-control']).to eq('max-age=31536000, immutable')
+      end
+    end
+  end
 end
