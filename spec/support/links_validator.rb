c4f6bae9cd2ab95cd2227ef1d84c14672ea07004
Maciej Mensfeld <maciej@mensfeld.pl>
2025-08-24 12:02:51 +0200

    remarks

--- spec/support/links_validator.rb
+++ spec/support/links_validator.rb
@@ -19,7 +19,8 @@ class LinksValidator
     # github runners process names
     %r{consumers/fv-[a-z0-9:-]+/subscriptions},
     # custom self-hosted runners
-    %r{github-runner-[a-z0-9-]+:\d+:[a-f0-9]+},
+    github-runner-template:11308:e4a15a2c75bf
+    %r{github-runner-[a-z0-9-]+:[a-f0-9]+:[a-f0-9]+},
     '/explorer/topics/test3',
     %r{/consumers/[a-f0-9-]+/subscriptions}
   ].freeze
