6e607db566bad5428da7a46427108e85a6ba2f81
Maciej Mensfeld <maciej@mensfeld.pl>
2025-09-09 22:39:12 +0200

    specs improvements (#772)

--- spec/lib/karafka/web/pro/ui/controllers/status_controller_spec.rb
+++ spec/lib/karafka/web/pro/ui/controllers/status_controller_spec.rb
@@ -33,5 +33,47 @@ RSpec.describe_current do
         expect(body).to include(breadcrumbs)
       end
     end
+
+    context 'when topics exist with data' do
+      let(:states_topic) { create_topic }
+      let(:metrics_topic) { create_topic }
+      let(:reports_topic) { create_topic }
+      let(:errors_topic) { create_topic }
+
+      before do
+        topics_config.consumers.states.name = states_topic
+        topics_config.consumers.metrics.name = metrics_topic
+        topics_config.consumers.reports.name = reports_topic
+        topics_config.errors.name = errors_topic
+
+        ::Karafka::Web::Management::Actions::CreateInitialStates.new.call
+        produce(metrics_topic, Fixtures.consumers_metrics_file)
+        ::Karafka::Web::Management::Actions::MigrateStatesData.new.call
+
+        get 'status'
+      end
+
+      it 'displays successful status with topic information' do
+        expect(response).to be_ok
+        expect(body).to include('Status')
+        expect(body).not_to include(support_message)
+        expect(body).to include(breadcrumbs)
+        expect(body).to include(states_topic)
+        expect(body).to include(metrics_topic)
+        expect(body).to include(reports_topic)
+        expect(body).to include(errors_topic)
+      end
+
+      it 'shows connection details' do
+        expect(body).to include('Components info')
+        expect(body).to include('rdkafka')
+        expect(body).to include('karafka')
+      end
+
+      it 'shows version information' do
+        expect(body).to include(::Karafka::VERSION)
+        expect(body).to include(::Karafka::Web::VERSION)
+      end
+    end
   end
 end
