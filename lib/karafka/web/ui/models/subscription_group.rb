13daf85afa9bb789f462fc81537571046b02dae5
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-14 21:46:43 +0100

    Remove redundant instance_id method from SubscriptionGroup model
    Now that instance_id is always set to either false or a string (never
    nil), HashProxy's method_missing works correctly and the explicit
    method is no longer needed.

--- lib/karafka/web/ui/models/subscription_group.rb
+++ lib/karafka/web/ui/models/subscription_group.rb
@@ -7,13 +7,6 @@ module Karafka
       module Models
         # Representation of data of a Karafka subscription group
         class SubscriptionGroup < Lib::HashProxy
-          # @return [String, false] the group.instance.id for static group membership, or false if
-          #   not configured. We need an explicit method because HashProxy#deep_find returns nil
-          #   for both missing keys and keys with nil values, causing method_missing to raise.
-          def instance_id
-            self[:instance_id]
-          end
-
           # @return [Array<Topic>] Data of topics belonging to this subscription group
           def topics
             super.values.map do |topic_hash|
