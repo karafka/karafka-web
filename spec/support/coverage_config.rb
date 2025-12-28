f91c5b5e069218a77cebe939b09b69a4434a468d
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-28 12:28:18 +0100

    Increase coverage thresholds to 96% line and 85% branch
    - Add spec/support/coverage_config.rb as single source of truth
    - Update spec_helper.rb to use shared config
    - Update bin/check_coverage to use shared config

---
+++ spec/support/coverage_config.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Coverage thresholds used by both spec_helper.rb and bin/check_coverage
+# Single source of truth for coverage requirements
+module CoverageConfig
+  LINE_COVERAGE = 96
+  BRANCH_COVERAGE = 85
+end
