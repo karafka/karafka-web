822495f1ad959d89157f75e9179f5d1777498da3
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-28 11:52:37 +0100

    Move spec timing checks into loader_spec.rb
    - Consolidate timing validation with license comment checks
    - Fix line length issues
    - Remove separate spec_timings_spec.rb file

--- spec/lib/karafka/web/pro/loader_spec.rb
+++ spec/lib/karafka/web/pro/loader_spec.rb
@@ -26,4 +26,49 @@ RSpec.describe_current do
       it { expect(content).to include(see) }
     end
   end
+
+  # Validates that all spec files have timing data for parallel test balancing.
+  # If this fails, run: bin/collect_timings
+  describe 'spec timings' do
+    let(:timings_dir) { Karafka::Web.gem_root.join('spec', 'timings') }
+
+    describe 'regular specs' do
+      let(:timings) { JSON.parse(File.read(timings_dir.join('regular.json'))) }
+
+      let(:spec_files) do
+        Dir[Karafka::Web.gem_root.join('spec', 'lib', 'karafka', 'web', '**', '*_spec.rb')]
+          .reject { |f| f.include?('/pro/') }
+          .map { |f| f.sub("#{Karafka::Web.gem_root}/", '') }
+      end
+
+      it 'has timing data for all regular spec files' do
+        timing_keys = timings.keys.map { |k| k.delete_prefix('./') }
+        missing = spec_files.reject { |f| timing_keys.include?(f) }
+
+        expect(missing).to be_empty,
+                           "Missing timing data for #{missing.size} files.\n" \
+                           "Run: bin/collect_timings\n" \
+                           "Missing:\n  #{missing.join("\n  ")}"
+      end
+    end
+
+    describe 'pro specs' do
+      let(:timings) { JSON.parse(File.read(timings_dir.join('pro.json'))) }
+
+      let(:spec_files) do
+        Dir[Karafka::Web.gem_root.join('spec', 'lib', 'karafka', 'web', 'pro', '**', '*_spec.rb')]
+          .map { |f| f.sub("#{Karafka::Web.gem_root}/", '') }
+      end
+
+      it 'has timing data for all pro spec files' do
+        timing_keys = timings.keys.map { |k| k.delete_prefix('./') }
+        missing = spec_files.reject { |f| timing_keys.include?(f) }
+
+        expect(missing).to be_empty,
+                           "Missing timing data for #{missing.size} files.\n" \
+                           "Run: bin/collect_timings\n" \
+                           "Missing:\n  #{missing.join("\n  ")}"
+      end
+    end
+  end
 end
