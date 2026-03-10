cbb935ca9346d41115b3a504edd75e6355009bd3
Maciej Mensfeld <maciej@mensfeld.pl>
2026-03-10 12:51:21 +0100

    Raise minimum line coverage threshold from 96% to 97%

--- test/support/coverage_config.rb
+++ test/support/coverage_config.rb
@@ -3,6 +3,6 @@
 # Coverage thresholds used by both test_helper.rb and bin/check_coverage
 # Single source of truth for coverage requirements
 module CoverageConfig
-  LINE_COVERAGE = 96
+  LINE_COVERAGE = 97
   BRANCH_COVERAGE = 85
 end
