63f85af5c7275fc2db195789db48344267f565fd
Maciej Mensfeld <maciej@mensfeld.pl>
2026-05-20 10:07:39 +0200

    Replace through2 with Node.js built-in stream module
    through2 v5 is ESM-only which breaks CommonJS require() usage in gulpfile.js.
    Replace through2.obj(fn) with Transform and through2.obj() with PassThrough
    from Node's built-in stream module, removing the external dependency entirely.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
+const { Transform, PassThrough } = require('stream');
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const through2 = require('through2');
 const path = require('path');
 const sourcemaps = require('gulp-sourcemaps');
 
@@ -25,20 +25,23 @@ const cssFiles = [
 
 // Custom transform stream to add file location comments for JavaScript
 function addFileLocation() {
-  return through2.obj(function (file, enc, cb) {
-    if (file.isBuffer()) {
-      const fileLocationComment = `/*! Source: ${path.relative(__dirname, file.path)} */\n\n`;
-      const contents = Buffer.concat([Buffer.from(fileLocationComment), file.contents]);
-      file.contents = contents;
+  return new Transform({
+    objectMode: true,
+    transform(file, enc, cb) {
+      if (file.isBuffer()) {
+        const fileLocationComment = `/*! Source: ${path.relative(__dirname, file.path)} */\n\n`;
+        const contents = Buffer.concat([Buffer.from(fileLocationComment), file.contents]);
+        file.contents = contents;
+      }
+      cb(null, file);
     }
-    cb(null, file);
   });
 }
 
 // JavaScript task
 gulp.task('scripts', function() {
   return gulp.src(jsFiles)
-    .pipe(isDev ? sourcemaps.init() : through2.obj()) // Initialize sourcemaps in dev
+    .pipe(isDev ? sourcemaps.init() : new PassThrough({ objectMode: true })) // Initialize sourcemaps in dev
     .pipe(addFileLocation()) // Add file location comments
     .pipe(concat('application.min.js'))
     .pipe(uglify({
@@ -54,16 +57,16 @@ gulp.task('scripts', function() {
         }
       }
     }))
-    .pipe(isDev ? sourcemaps.write('.') : through2.obj()) // Write sourcemaps in dev
+    .pipe(isDev ? sourcemaps.write('.') : new PassThrough({ objectMode: true })) // Write sourcemaps in dev
     .pipe(gulp.dest('lib/karafka/web/ui/public/javascripts'));
 });
 
 // CSS task
 gulp.task('styles', function() {
   return gulp.src(cssFiles)
-    .pipe(isDev ? sourcemaps.init() : through2.obj()) // Initialize sourcemaps in dev
+    .pipe(isDev ? sourcemaps.init() : new PassThrough({ objectMode: true })) // Initialize sourcemaps in dev
     .pipe(concat('application.min.css'))
-    .pipe(isDev ? sourcemaps.write('.') : through2.obj()) // Write sourcemaps in dev
+    .pipe(isDev ? sourcemaps.write('.') : new PassThrough({ objectMode: true })) // Write sourcemaps in dev
     .pipe(gulp.dest('lib/karafka/web/ui/public/stylesheets'));
 });
 
