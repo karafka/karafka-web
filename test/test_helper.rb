435b2631a25e037dfb12d5713eb36f61318be280
Maciej Mensfeld <maciej@mensfeld.pl>
2026-06-03 10:36:40 +0200

    Enable deprecated warnings in tests
    Signed-off-by: Maciej Mensfeld <maciej@mensfeld.pl>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,7 @@ require "warning"
 # Enable all opt-in warning categories. Warning.categories is available
 # since Ruby 3.4; on older Rubies this is a no-op.
 if Warning.respond_to?(:categories)
-  (Warning.categories - %i[deprecated experimental]).each { |cat| Warning[cat] = true }
+  (Warning.categories - %i[experimental]).each { |cat| Warning[cat] = true }
 end
 
 Warning.process do |warning|
